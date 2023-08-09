0b22afb0f4062cbe16c147ae38dedef1a2bfe85f
madandinata <madanmr48@gmail.com>
2023-08-09 17:07:21 +0700

    added home and detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom"
 import "./App.css"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Detail from "./pages/Detail"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-center font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   )
 }
 
